4d796568e165eb2a03e5d8e1ab52776b56921149
okatoi-py <king05jadem@icloud.com>
2026-04-13 16:05:51 -0400

    added Window and Globals class

---
+++ src/sheepsheepmania/Globals.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sheepsheepmania;
+
+// Jadem King
+// 4/13/2026
+// Globals class for global vars
+
+
+public class Globals {
+    
+}
---
+++ src/sheepsheepmania/Window.java
@@ -0,0 +1,27 @@
+
+package sheepsheepmania;
+
+import javax.swing.*;
+import java.awt.*;
+
+//Jadem King
+// 4/13/2026
+//Creating a window using JFrame
+
+
+public class Window extends JFrame{
+    
+    Window(){
+        
+        
+        
+        
+        this.setVisible(true);
+        
+        
+    }
+    
+    
+    
+    
+}
